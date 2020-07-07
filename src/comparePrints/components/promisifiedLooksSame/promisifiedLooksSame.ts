f62d6c1a1bb6795c6ecec785330d54a10d467966
rap0so <william.s.godoy53@gmail.com>
2020-07-07 18:04:06 -0300

    test: ensure comparePrints

--- src/comparePrints/components/promisifiedLooksSame/promisifiedLooksSame.ts
+++ src/comparePrints/components/promisifiedLooksSame/promisifiedLooksSame.ts
@@ -8,7 +8,6 @@ const promisifiedLooksSame = (firstImgUrl: string, secondImgUrl: string) => new 
         return resolve(true);
       }
 
-      // TODO: add server url to the img, so we can see the img by url
       return reject(new Error(`Image ${firstImgUrl} and ${secondImgUrl} not match`));
     })),
 );
