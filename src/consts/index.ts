8f8e26fe6cf97b59d88298e873da652507dadea2
rap0so <william.s.godoy53@gmail.com>
2020-07-10 09:19:37 -0300

    chore(env): configure PORT env

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,3 +1,1 @@
-export const serverPort = 9000;
-
 export const printDir = 'prints';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 
 import main from './main';
-import { serverPort } from './consts'
 
-http.createServer(main).listen(serverPort, () => console.log('Server ON'));
+const port = process.env.PORT || 9000
+
+http.createServer(main).listen(port, () => console.log('Server ON'));
