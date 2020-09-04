edda2e92ace9c2dc9a64f13286fd0c81eb5e4bd2
rap0so <william.s.godoy53@gmail.com>
2020-09-04 14:05:52 -0300

    test(main): mock sendMsgToSlack response

--- src/main/main.test.ts
+++ src/main/main.test.ts
@@ -2,7 +2,23 @@ import http from 'http';
 
 import main from '.';
 
+jest.mock('@slack/web-api', () => ({
+  WebClient: function mockWebClient() {
+    return {
+      chat: {
+        postMessage: jest.fn().mockImplementation(() => ({
+          ts: 'sent',
+        })),
+      },
+    };
+  },
+}));
+
 describe('main', () => {
+  beforeEach(() => {
+    process.env.SLACK_LOG_CHANNEL_ID = '1';
+  });
+
   it('Should run without break', async () => {
     const fakeResponse = { end: jest.fn } as unknown as http.ServerResponse;
 
--- src/sendMsgToSlack/sendMsgToSlack.ts
+++ src/sendMsgToSlack/sendMsgToSlack.ts
@@ -1,18 +1,17 @@
 import { WebClient } from '@slack/web-api';
 
-// TODO: test it
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+
 const sendMsgToSlack = async (scope: 'log'|'warning', msg: string) => {
   try {
-    const token = process.env.SLACK_TOKEN;
-    const web = new WebClient(token);
-
     const conversationId = scope === 'log' ? process.env.SLACK_LOG_CHANNEL_ID : process.env.SLACK_WARNING_CHANNEL_ID;
+
     if (!conversationId) {
       throw new Error('No conversationId configured');
     }
 
     const res = await web.chat.postMessage({ channel: conversationId, text: msg });
-
     return console.log('Message sent: ', res.ts);
   } catch (error) {
     return console.log('Error at send messages:', error);
